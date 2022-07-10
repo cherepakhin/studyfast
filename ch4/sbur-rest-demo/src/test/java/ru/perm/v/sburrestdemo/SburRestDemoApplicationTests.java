26bdb1dad70f8f8566571b69ca61f21020f05460
Черепахин Василий <vasi.che@gmail.com>
2022-07-10 15:24:51 +0500

    Добавил все файлы Главы 4 в maven

---
+++ ch4/sbur-rest-demo/src/test/java/ru/perm/v/sburrestdemo/SburRestDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.perm.v.sburrestdemo;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class SburRestDemoApplicationTests {
+
+	@Test
+	void simple() {
+		// simple test
+		assertTrue(true);
+	}
+
+}
