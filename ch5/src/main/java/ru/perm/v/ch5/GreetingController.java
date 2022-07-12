e27a0a41c8617688d9cd343df8e40e2b32f42198
Черепахин Василий <vasi.che@gmail.com>
2022-07-12 13:42:35 +0500

    Добавлен контроллер GreetingController

---
+++ ch5/src/main/java/ru/perm/v/ch5/GreetingController.java
@@ -0,0 +1,18 @@
+package ru.perm.v.ch5;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/greeting")
+public class GreetingController {
+    @Value("${greeting-name: Mirage}") // greeting-name: Mirage работает, если не задано в application.properies
+    private String name;
+
+    @GetMapping("/")
+    public String getGreeting() {
+        return name;
+    }
+}
