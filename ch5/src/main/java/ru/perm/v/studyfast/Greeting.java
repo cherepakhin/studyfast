c8aaea31d19ad8b9683cf88f51659cc147837488
vasi <vasi.che@gmail.com>
2022-07-29 15:52:09 +0500

    Удален остаток от прежней конф-ии (prefix = "greeting")

--- ch5/src/main/java/ru/perm/v/studyfast/Greeting.java
+++ ch5/src/main/java/ru/perm/v/studyfast/Greeting.java
@@ -3,7 +3,7 @@ package ru.perm.v.studyfast;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties(prefix = "greeting")
+@ConfigurationProperties
 public class Greeting {
     @Value("${greeting-name: Mirage}")
     private String name;
